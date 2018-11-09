41e4e663962b4d7c16e804dba2cce47ac82fd66b
Erik Kranzusch <erikk@spindance.com>
2018-11-09 13:06:48 -0500

    remove zenoss intents, and change brucej

--- embot-core/src/intents/mCloseZenossEvent.ts
+++
@@ -1,9 +0,0 @@
-import * as HTTP from 'http'
-import { json } from 'micro'
-
-module.exports = async (req: HTTP.IncomingMessage, res: HTTP.ServerResponse) => {
-    let body = await json(req)
-
-    res.end('You triggered the CloseZenossEvent Intent!')
-    return
-}
